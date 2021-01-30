2f303e5ef8c3ab5ed6aa1c49e137d941b0b7a6af
marizona <marie.jacquier@epitech.eu>
2021-01-30 20:49:05 +0100

    UI nav, login, register an Chat

--- front_swapit/src/App.js
+++ front_swapit/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { Navbar } from "./components/navbar";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Login} from "./components/Pages/Login";
+import { Register } from "./components/Pages/Register";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import Chat from "./Chat";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <Router>
+        <Navbar />
+
+        <div className="pages">
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+          
+          </Switch>
+        </div>
+      </Router>
+
+
+
+
+
+
+      <SignUp/>
+      <SignIn/>
+      <Chat/>
+    </>
   );
 }
 
