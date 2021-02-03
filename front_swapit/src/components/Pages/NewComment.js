5cb1dc4f989caf7513aa393c4eecf42c4d0c9fc8
Vincent CAstel <vincent.castel@epitech.eu>
2021-02-03 15:13:53 +0100

    front post fonctionne, ajout du titre à tester avec le back

--- front_swapit/src/components/Pages/NewComment.js
+++ front_swapit/src/components/Pages/NewComment.js
@@ -9,50 +9,61 @@ import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import axios from 'axios';
 import url from '../../url.js'
 
-const API_URL = url + "post_comment"
-/*STYLES */
-const useStyles = makeStyles((theme) => ({
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
-    textarea:{
-        borderRadius: "4px",
-        border:"solid 1px rgb(200,200,200)",
-        width:"100%"
-    },
-    input:{
-        width:"100%"
-    },
-    form:{
-        padding:"1em"
-    }
-}));
-/*PREPARE TO SEND */
-const handleChange={
-    
-}
+/*
+NewComment component need to receive twos props :
+-comment_author
+-comment_target
 
-/* POST  */
-const SEND = event => {
-    event.preventDefault();
-    var commentForm = document.getElementById('commentForm');
-    var formData = new FormData(commentForm);
-    console.log(formData);
-    axios.post(API_URL, { formData })
-      .then(res => {
-        console.log(res);
-        console.log(res.data);
-      }); 
-};
-/*DOM */
+NewComment use url.js file
+*/
+
 const NewComment = (props) => {
+        
+    /*STYLES */
+    const useStyles = makeStyles((theme) => ({
+        submit: {
+            margin: theme.spacing(3, 0, 2),
+        },
+        textarea:{
+            borderRadius: "4px",
+            border:"solid 1px rgb(200,200,200)",
+            width:"100%"
+        },
+        input:{
+            width:"100%"
+        },
+        form:{
+            padding:"1em"
+        }
+    }));
+
+    /* POST  */
+    const API_URL = url + "post_comment"
+
+    const SEND = event => {
+        event.preventDefault();
+        var commentForm = document.getElementById('commentForm');
+        var formData = new FormData(commentForm);
+        axios({
+            method: 'post',
+            url: API_URL,
+            data: formData
+        })
+        .then(res => {
+            console.log(res);
+            console.log(res.data);
+        }); 
+    };
+
+    /*DOM */
+
     const classes = useStyles();
     const [value, setValue]=React.useState(4);//allow to not create class. 4 =default value
     var val = value;//value to send for the rating
     return (
         <div>
             <form class={classes.form} id="commentForm">
-                <input placeholder="Titre du commentaire" className={classes.input}></input>
+                <input placeholder="Titre du commentaire" className={classes.input} nom ="title"></input>
                 <input value={val} type="hidden" name="ratings"></input>
                 <input value={props.comment_author} type="hidden" name="comment_author"></input>
                 <input value={props.comment_target} type="hidden" name="comment_target"></input>
@@ -66,7 +77,7 @@ const NewComment = (props) => {
                         }}
                     />
                 </Box>
-                <TextareaAutosize  placeholder="Ecrivez-votre commentaire ici" rowsMin={6} className={classes.textarea} name="content" onChange={handleChange}/>
+                <TextareaAutosize  placeholder="Ecrivez-votre commentaire ici" rowsMin={6} className={classes.textarea} name="content"/>
                 <Button 
                     onClick={SEND}
                     type="submit"
