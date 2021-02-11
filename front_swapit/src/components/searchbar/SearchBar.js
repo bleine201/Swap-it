d85d337c9e80155ea33e70a74deca6f43baed3be
romain <romain.galtier@epitech.eu>
2021-02-11 12:44:52 +0100

    axios api

--- front_swapit/src/components/searchbar/SearchBar.js
+++ front_swapit/src/components/searchbar/SearchBar.js
@@ -4,6 +4,7 @@ import SearchIcon from '@material-ui/icons/Search';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
 
 
@@ -11,49 +12,74 @@ class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      term: '',
+      error: null,
+      isLoaded: false,
+      terms: []
     };
   }
-  onInputChange(term) {
+  componentDidMount() {
+    axios.get(`http://127.0.0.1:8000/api/search/{name}`)
+      .then(res => {
+       // console.log(res);
+        this.setState({
+          isLoaded: true,
+          terms: res.terms
+        });
+      })
+      .catch(error => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      })
+  }
+  onInputChange(terms) {
     const name = this.props.searchBoxName || undefined
-    this.setState({ term });
+    this.setState({ terms });
     if (this.props.onSearchTermChange) {
-      this.props.onSearchTermChange({ name, term })
+      this.props.onSearchTermChange({ name, terms })
     }
   }
   searchResult() {
-    const result = this.state.term
-    //console.log(result);
+    const result = this.state.terms
+    console.log(result);
   }
 
   render() {
-    const name = this.props.searchBoxName || undefined
-    return (
-      <div className="search-box">
-        <TextField
-          variant="outlined"
-          name={name}
-          className="search-input"
-          id="search"
-          type="text"
-          placeholder="Search..."
-          value={this.state.term}
-          onChange={event => this.onInputChange(event.target.value)}
-          onKeyPress={this.props.onKeyPress || null}
+    const { error, isLoaded, terms, name } = this.state;
+    if (error) {
+      return <div>Erreur : {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Chargement…</div>;
+    } else {
+      return (
+        <div className="search-box">
+          <TextField
+            terms={terms}
+            variant="outlined"
+            name={name}
+            className="search-input"
+            id="search"
+            type="text"
+            placeholder="Search..."
+            value={this.state.terms}
+            onChange={event => this.onInputChange(event.target.value)}
+            onKeyPress={this.props.onKeyPress || null}
 
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <Button
-                  onClick={() => this.searchResult()}
-                  startIcon={<SearchIcon />}>
-                </Button>
-              </InputAdornment>
-            )
-          }}
-        />
-      </div>
-    );
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Button
+                    onClick={() => this.searchResult()}
+                    startIcon={<SearchIcon />}>
+                  </Button>
+                </InputAdornment>
+              )
+            }}
+          />
+        </div>
+      );
+    }
   }
 }
 
